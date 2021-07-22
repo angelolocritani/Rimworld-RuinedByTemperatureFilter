730de7ca9921c0e696e7a7acfea3add55f753132
angelolocritani <67960642+angelolocritani@users.noreply.github.com>
2021-07-22 22:36:49 +0200

    Initial commit
    First commit for RuinedByTemperature mod for Rimworld

---
+++ Source/RuinedByTemperatureFilter/RuinedByTemperatureFilterMod.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace RuinedByTemperatureFilter
+{
+    public class RuinedByTemperatureFilterMod : Mod
+    {
+        public RuinedByTemperatureFilterMod(ModContentPack content) : base(content) { }
+    }
+    public class SpecialThingFilterWorker_RuinedByTemperature : SpecialThingFilterWorker
+    {
+        public override bool Matches(Thing t) => CanEverMatch(t.def) && t.TryGetComp<CompTemperatureRuinable>().Ruined;
+
+        public override bool CanEverMatch(ThingDef def) => def.IsEgg && def.HasComp(typeof(CompTemperatureRuinable));
+
+      
+    }
+
+    public class SpecialThingFilterWorker_NotRuinedByTemperature : SpecialThingFilterWorker
+    {
+        public override bool Matches(Thing t) => CanEverMatch(t.def) && !t.TryGetComp<CompTemperatureRuinable>().Ruined;
+
+        public override bool CanEverMatch(ThingDef def) => def.IsEgg && def.HasComp(typeof(CompTemperatureRuinable));
+
+
+    }
+}
